0ba39321a8811bbdf0ba04d00f65f86b8696ab1c
Andres Freund <andres@anarazel.de>
2012-11-12 13:39:52 +0100

    wal_decoding: Adjust all *Satisfies routines to take a HeapTuple instead of a HeapTupleHeader
    For the regular satisfies routines this is needed in prepareation of logical
    decoding. I changed the non-regular ones for consistency as well.
    
    The naming between htup, tuple and similar is rather confused, I could not find
    any consistent naming anywhere.
    
    This is preparatory work for the logical decoding feature which needs to be
    able to get to a valid relfilenode from when checking the visibility of a
    tuple.

--- contrib/pgrowlocks/pgrowlocks.c
+++ contrib/pgrowlocks/pgrowlocks.c
@@ -120,7 +120,7 @@ pgrowlocks(PG_FUNCTION_ARGS)
 		/* must hold a buffer lock to call HeapTupleSatisfiesUpdate */
 		LockBuffer(scan->rs_cbuf, BUFFER_LOCK_SHARE);
 
-		if (HeapTupleSatisfiesUpdate(tuple->t_data,
+		if (HeapTupleSatisfiesUpdate(tuple,
 									 GetCurrentCommandId(false),
 									 scan->rs_cbuf) == HeapTupleBeingUpdated)
 		{
