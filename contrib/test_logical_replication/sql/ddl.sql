7f1e6c2aaeb17f2ee33fc88de5e06a6057634751
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2013-01-11 14:36:48 +0530

    wal_decoding: Add test_logical_replication extension for easier testing of logical decoding
    This extension provides three functions for manipulating replication slots:
    * init_logical_replication - initiate a replication slot and wait for consistent state
    * start_logical_replication - return all changes since the last call up to now, without blocking
    * free_logical_replication - free the logical slot again
    
    Those are pretty direct synonyms for the replication connection commands.
    
    Due to questions about how to integrate logical replication tests this module
    also contains the current tests of logical replication itself.
    
    Author: Abhijit Menon-Sen

---
+++ contrib/test_logical_replication/sql/ddl.sql
@@ -0,0 +1,154 @@
+CREATE EXTENSION test_logical_replication;
+-- predictability
+SET synchronous_commit = on;
+
+-- faster startup
+CHECKPOINT;
+
+SELECT 'init' FROM init_logical_replication('test_decoding');
+
+CREATE TABLE replication_example(id SERIAL PRIMARY KEY, somedata int, text varchar(120));
+BEGIN;
+INSERT INTO replication_example(somedata, text) VALUES (1, 1);
+INSERT INTO replication_example(somedata, text) VALUES (1, 2);
+COMMIT;
+
+ALTER TABLE replication_example ADD COLUMN bar int;
+
+INSERT INTO replication_example(somedata, text, bar) VALUES (2, 1, 4);
+
+BEGIN;
+INSERT INTO replication_example(somedata, text, bar) VALUES (2, 2, 4);
+INSERT INTO replication_example(somedata, text, bar) VALUES (2, 3, 4);
+INSERT INTO replication_example(somedata, text, bar) VALUES (2, 4, NULL);
+COMMIT;
+
+ALTER TABLE replication_example DROP COLUMN bar;
+INSERT INTO replication_example(somedata, text) VALUES (3, 1);
+
+BEGIN;
+INSERT INTO replication_example(somedata, text) VALUES (3, 2);
+INSERT INTO replication_example(somedata, text) VALUES (3, 3);
+COMMIT;
+
+ALTER TABLE replication_example RENAME COLUMN text TO somenum;
+
+INSERT INTO replication_example(somedata, somenum) VALUES (4, 1);
+
+-- collect all changes
+SELECT data FROM start_logical_replication('now');
+
+ALTER TABLE replication_example ALTER COLUMN somenum TYPE int4 USING (somenum::int4);
+-- throw away changes, they contain oids
+SELECT count(data) FROM start_logical_replication('now');
+
+INSERT INTO replication_example(somedata, somenum) VALUES (5, 1);
+
+BEGIN;
+INSERT INTO replication_example(somedata, somenum) VALUES (6, 1);
+ALTER TABLE replication_example ADD COLUMN zaphod1 int;
+INSERT INTO replication_example(somedata, somenum, zaphod1) VALUES (6, 2, 1);
+ALTER TABLE replication_example ADD COLUMN zaphod2 int;
+INSERT INTO replication_example(somedata, somenum, zaphod2) VALUES (6, 3, 1);
+INSERT INTO replication_example(somedata, somenum, zaphod1) VALUES (6, 4, 2);
+COMMIT;
+
+/*
+ * check whether the correct indexes are chosen for deletions
+ */
+
+CREATE TABLE tr_unique(id2 serial unique NOT NULL, data int);
+INSERT INTO tr_unique(data) VALUES(10);
+--show deletion with unique index
+DELETE FROM tr_unique;
+
+ALTER TABLE tr_unique RENAME TO tr_pkey;
+
+-- show changes
+SELECT data FROM start_logical_replication('now');
+
+-- hide changes bc of oid visible in full table rewrites
+ALTER TABLE tr_pkey ADD COLUMN id serial primary key;
+SELECT count(data) FROM start_logical_replication('now');
+
+INSERT INTO tr_pkey(data) VALUES(1);
+--show deletion with primary key
+DELETE FROM tr_pkey;
+
+/* display results */
+SELECT data FROM start_logical_replication('now');
+
+/*
+ * check that disk spooling works
+ */
+BEGIN;
+CREATE TABLE tr_etoomuch (id serial primary key, data int);
+INSERT INTO tr_etoomuch(data) SELECT g.i FROM generate_series(1, 10234) g(i);
+DELETE FROM tr_etoomuch WHERE id < 5000;
+UPDATE tr_etoomuch SET data = - data WHERE id > 5000;
+COMMIT;
+
+/* display results, but hide most of the output */
+SELECT count(*), min(data), max(data)
+FROM start_logical_replication('now')
+GROUP BY substring(data, 1, 24)
+ORDER BY 1;
+
+/*
+ * check whether we subtransactions correctly in relation with each other
+ */
+CREATE TABLE tr_sub (id serial primary key, path text);
+
+-- toplevel, subtxn, toplevel, subtxn, subtxn
+BEGIN;
+INSERT INTO tr_sub(path) VALUES ('1-top-#1');
+
+SAVEPOINT a;
+INSERT INTO tr_sub(path) VALUES ('1-top-1-#1');
+INSERT INTO tr_sub(path) VALUES ('1-top-1-#2');
+RELEASE SAVEPOINT a;
+
+SAVEPOINT b;
+SAVEPOINT c;
+INSERT INTO tr_sub(path) VALUES ('1-top-2-1-#1');
+INSERT INTO tr_sub(path) VALUES ('1-top-2-1-#2');
+RELEASE SAVEPOINT c;
+INSERT INTO tr_sub(path) VALUES ('1-top-2-#1');
+RELEASE SAVEPOINT b;
+COMMIT;
+
+SELECT data FROM start_logical_replication('now');
+
+-- check that we handle xlog assignments correctly
+BEGIN;
+-- nest 80 subtxns
+SAVEPOINT subtop;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;SAVEPOINT a;
+-- assign xid by inserting
+INSERT INTO tr_sub(path) VALUES ('2-top-1...--#1');
+INSERT INTO tr_sub(path) VALUES ('2-top-1...--#2');
+INSERT INTO tr_sub(path) VALUES ('2-top-1...--#3');
+RELEASE SAVEPOINT subtop;
+INSERT INTO tr_sub(path) VALUES ('2-top-#1');
+COMMIT;
+
+SELECT data FROM start_logical_replication('now');
+
+
+-- done, free logical replication slot
+SELECT data FROM start_logical_replication('now');
+SELECT stop_logical_replication();
---
+++ contrib/test_logical_replication/test_logical_replication.c
@@ -0,0 +1,352 @@
+#include "postgres.h"
+
+#include "access/timeline.h"
+#include "access/xlog_internal.h"
+#include "catalog/pg_type.h"
+#include "libpq/pqformat.h"
+#include "replication/decode.h"
+#include "replication/logical.h"
+#include "replication/snapbuild.h"
+#include "storage/procarray.h"
+#include "utils/builtins.h"
+#include "utils/memutils.h"
+#include "utils/resowner.h"
+#include "utils/syscache.h"
+#include "miscadmin.h"
+#include "funcapi.h"
+
+PG_MODULE_MAGIC;
+
+Datum init_logical_replication(PG_FUNCTION_ARGS);
+Datum start_logical_replication(PG_FUNCTION_ARGS);
+Datum stop_logical_replication(PG_FUNCTION_ARGS);
+
+static const char *slot_name = NULL;
+static Tuplestorestate *tupstore = NULL;
+static TupleDesc tupdesc;
+
+extern void XLogRead(char *buf, TimeLineID tli, XLogRecPtr startptr, Size count);
+
+static int
+test_read_page(XLogReaderState* state, XLogRecPtr targetPagePtr, int reqLen,
+                 char* cur_page, TimeLineID *pageTLI)
+{
+    XLogRecPtr flushptr, loc;
+    int count;
+
+	loc = targetPagePtr + reqLen;
+	while (1) {
+		flushptr = GetFlushRecPtr();
+		if (loc <= flushptr)
+			break;
+		pg_usleep(1000L);
+	}
+
+    /* more than one block available */
+    if (targetPagePtr + XLOG_BLCKSZ <= flushptr)
+        count = XLOG_BLCKSZ;
+    /* not enough data there */
+    else if (targetPagePtr + reqLen > flushptr)
+        return -1;
+    /* part of the page available */
+    else
+        count = flushptr - targetPagePtr;
+
+    /* FIXME: more sensible/efficient implementation */
+    XLogRead(cur_page, ThisTimeLineID, targetPagePtr, XLOG_BLCKSZ);
+
+    return count;
+}
+
+static void store_tuple(XLogRecPtr ptr, TransactionId xid, StringInfo si)
+{
+	Datum values[3];
+	bool nulls[3];
+	char buf[60];
+
+	sprintf(buf, "%X/%X", (uint32)(ptr >> 32), (uint32)ptr);
+
+	memset(nulls, 0, sizeof(nulls));
+	values[0] = CStringGetTextDatum(buf);
+	values[1] = Int64GetDatum(xid);
+	values[2] = CStringGetTextDatum(si->data);
+
+	tuplestore_putvalues(tupstore, tupdesc, values, nulls);
+}
+
+static void
+begin_txn_wrapper(ReorderBuffer* cache, ReorderBufferTXN* txn)
+{
+	ReaderApplyState *state = cache->private_data;
+	bool send;
+
+	resetStringInfo(state->out);
+
+	send = state->begin_cb(state->user_private, state->out, txn);
+
+	if (send)
+		store_tuple(txn->lsn, txn->xid, state->out);
+}
+
+static void
+commit_txn_wrapper(ReorderBuffer* cache, ReorderBufferTXN* txn, XLogRecPtr commit_lsn)
+{
+	ReaderApplyState *state = cache->private_data;
+	bool send;
+
+	resetStringInfo(state->out);
+
+	send = state->commit_cb(state->user_private, state->out, txn, commit_lsn);
+
+	if (send)
+		store_tuple(commit_lsn, txn->xid, state->out);
+}
+
+static void
+change_wrapper(ReorderBuffer* cache, ReorderBufferTXN* txn, ReorderBufferChange* change)
+{
+	ReaderApplyState *state = cache->private_data;
+	bool send;
+	HeapTuple table;
+	Oid reloid;
+
+	resetStringInfo(state->out);
+
+	table = LookupRelationByRelFileNode(&change->relnode);
+	Assert(table);
+	reloid = HeapTupleHeaderGetOid(table->t_data);
+	ReleaseSysCache(table);
+
+	send = state->change_cb(state->user_private, state->out, txn,
+							reloid, change);
+
+	if (send)
+		store_tuple(change->lsn, txn->xid, state->out);
+}
+
+
+PG_FUNCTION_INFO_V1(init_logical_replication);
+
+Datum
+init_logical_replication(PG_FUNCTION_ARGS)
+{
+	const char *plugin;
+	char		xpos[MAXFNAMELEN];
+	XLogReaderState *logical_reader;
+
+	TupleDesc   tupdesc;
+	HeapTuple   tuple;
+	Datum       result;
+	Datum       values[2];
+	bool        nulls[2];
+
+	if (slot_name)
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 (errmsg("sorry, can't init logical replication twice"))));
+
+	if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+		elog(ERROR, "return type must be a row type");
+
+	/* Acquire a logical replication slot */
+	plugin = text_to_cstring(PG_GETARG_TEXT_P(0));
+	CheckLogicalReplicationRequirements();
+	LogicalDecodingAcquireFreeSlot(plugin);
+
+	/*
+	 * Use the same initial_snapshot_reader, but with our own read_page
+	 * callback that does not depend on walsender.
+	 */
+	logical_reader = initial_snapshot_reader(MyLogicalDecodingSlot->last_required_checkpoint,
+											 MyLogicalDecodingSlot->xmin,
+											 NameStr(MyLogicalDecodingSlot->plugin));
+	logical_reader->read_page = test_read_page;
+
+	/* Wait for a consistent starting point */
+	for (;;)
+	{
+		XLogRecord *record;
+		XLogRecordBuffer buf;
+		ReaderApplyState* apply_state = logical_reader->private_data;
+		char *err = NULL;
+
+		/* the read_page callback waits for new WAL */
+		record = XLogReadRecord(logical_reader, InvalidXLogRecPtr, &err);
+		if (err)
+			elog(ERROR, "%s", err);
+
+		Assert(record);
+
+		buf.origptr = logical_reader->ReadRecPtr;
+		buf.record = *record;
+		buf.record_data = XLogRecGetData(record);
+		DecodeRecordIntoReorderBuffer(logical_reader, apply_state, &buf);
+
+		if (initial_snapshot_ready(logical_reader))
+			break;
+	}
+
+	/* Extract the values we want */
+	MyLogicalDecodingSlot->confirmed_flush = logical_reader->EndRecPtr;
+	slot_name = NameStr(MyLogicalDecodingSlot->name);
+	snprintf(xpos, sizeof(xpos), "%X/%X",
+			 (uint32) (MyLogicalDecodingSlot->confirmed_flush >> 32),
+			 (uint32) MyLogicalDecodingSlot->confirmed_flush);
+
+	/* Release the slot and return the values */
+	LogicalDecodingReleaseSlot();
+
+	values[0] = CStringGetTextDatum(slot_name);
+	values[1] = CStringGetTextDatum(xpos);
+
+	memset(nulls, 0, sizeof(nulls));
+
+	tuple = heap_form_tuple(tupdesc, values, nulls);
+	result = HeapTupleGetDatum(tuple);
+
+	PG_RETURN_DATUM(result);
+}
+
+PG_FUNCTION_INFO_V1(start_logical_replication);
+
+Datum
+start_logical_replication(PG_FUNCTION_ARGS)
+{
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	MemoryContext per_query_ctx;
+	MemoryContext oldcontext;
+
+	XLogRecPtr now;
+	XLogReaderState *logical_reader;
+	ReaderApplyState *apply_state;
+	ReorderBuffer *reorder;
+
+	ResourceOwner old_resowner = CurrentResourceOwner;
+
+	if (!slot_name)
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+				 (errmsg("sorry, can't start logical replication outside of an init/stop pair"))));
+
+	/* check to see if caller supports us returning a tuplestore */
+	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("set-valued function called in context that cannot accept a set")));
+	if (!(rsinfo->allowedModes & SFRM_Materialize))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("materialize mode required, but it is not " \
+						"allowed in this context")));
+
+	/* Build a tuple descriptor for our result type */
+	if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+		elog(ERROR, "return type must be a row type");
+
+	per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+	oldcontext = MemoryContextSwitchTo(per_query_ctx);
+
+	tupstore = tuplestore_begin_heap(true, false, work_mem);
+	rsinfo->returnMode = SFRM_Materialize;
+	rsinfo->setResult = tupstore;
+	rsinfo->setDesc = tupdesc;
+
+	MemoryContextSwitchTo(oldcontext);
+
+	/*
+	 * XXX: It's impolite to ignore our argument and keep decoding
+	 * until the current position.
+	 */
+	now = GetFlushRecPtr();
+
+	/*
+	 * We need to create a normal_snapshot_reader, but adjust it to use
+	 * our page_read callback, and also make its reorder buffer use our
+	 * callback wrappers that don't depend on walsender.
+	 */
+
+	CheckLogicalReplicationRequirements();
+	LogicalDecodingReAcquireSlot(slot_name);
+	logical_reader = normal_snapshot_reader(MyLogicalDecodingSlot->last_required_checkpoint,
+											MyLogicalDecodingSlot->xmin,
+											NameStr(MyLogicalDecodingSlot->plugin),
+											MyLogicalDecodingSlot->confirmed_flush);
+
+	logical_reader->read_page = test_read_page;
+	apply_state = (ReaderApplyState *)logical_reader->private_data;
+
+	reorder = apply_state->reorderbuffer;
+	reorder->begin = begin_txn_wrapper;
+	reorder->apply_change = change_wrapper;
+	reorder->commit = commit_txn_wrapper;
+
+	elog(DEBUG1, "Starting logical replication from %X/%X to %X/%x",
+		 (uint32)(MyLogicalDecodingSlot->last_required_checkpoint>>32), (uint32)MyLogicalDecodingSlot->last_required_checkpoint,
+		 (uint32)(now>>32), (uint32)now);
+
+	CurrentResourceOwner = ResourceOwnerCreate(CurrentResourceOwner, "logical decoding");
+
+	for (;;)
+	{
+		XLogRecord *record;
+		char *errm = NULL;
+
+		record = XLogReadRecord(logical_reader, InvalidXLogRecPtr, &errm);
+		if (errm)
+			elog(ERROR, "%s", errm);
+
+		if (record != NULL)
+		{
+			XLogRecPtr rp;
+			XLogRecordBuffer buf;
+			ReaderApplyState* apply_state = logical_reader->private_data;
+
+			buf.origptr = logical_reader->ReadRecPtr;
+			buf.record = *record;
+			buf.record_data = XLogRecGetData(record);
+
+			/*
+			 * The {begin_txn,change,commit_txn}_wrapper callbacks above
+			 * will store the description into our tuplestore.
+			 */
+			DecodeRecordIntoReorderBuffer(logical_reader, apply_state, &buf);
+
+			rp = logical_reader->EndRecPtr;
+			if (rp >= now)
+			{
+				elog(DEBUG1, "Reached endpoint (wanted: %X/%X, got: %X/%X)",
+					 (uint32)(now>>32), (uint32)now,
+					 (uint32)(rp>>32), (uint32)rp);
+				break;
+			}
+		}
+	}
+
+	tuplestore_donestoring(tupstore);
+
+	CurrentResourceOwner = old_resowner;
+
+	/* Next time, start where we left off */
+	MyLogicalDecodingSlot->confirmed_flush = logical_reader->EndRecPtr;
+
+	LogicalDecodingReleaseSlot();
+
+	return (Datum) 0;
+}
+
+PG_FUNCTION_INFO_V1(stop_logical_replication);
+
+Datum
+stop_logical_replication(PG_FUNCTION_ARGS)
+{
+	if (!slot_name)
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 (errmsg("sorry, can't stop logical replication before init"))));
+
+	CheckLogicalReplicationRequirements();
+	LogicalDecodingFreeSlot(slot_name);
+	slot_name = NULL;
+
+	PG_RETURN_INT32(0);
+}
