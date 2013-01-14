c832ce39b50b2949dd6df0b56ccee3604746c51a
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2013-01-14 18:26:39 +0530

    None of these functions is IMMUTABLE

--- contrib/test_logical_replication/test_logical_replication--1.0.sql
+++ contrib/test_logical_replication/test_logical_replication--1.0.sql
@@ -3,12 +3,12 @@
 
 CREATE FUNCTION init_logical_replication (text, OUT slot_name text, OUT xlog_position text)
 AS 'MODULE_PATHNAME', 'init_logical_replication'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C STRICT;
 
 CREATE FUNCTION start_logical_replication (text) RETURNS SETOF TEXT
 AS 'MODULE_PATHNAME', 'start_logical_replication'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C STRICT;
 
 CREATE FUNCTION stop_logical_replication () RETURNS int
 AS 'MODULE_PATHNAME', 'stop_logical_replication'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C STRICT;
