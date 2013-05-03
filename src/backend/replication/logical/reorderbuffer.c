2ndQuadrant/bdr

wal_decoding: reorderbuffer: Track original commit timestamp of a transaction

src/backend/replication/logical/reorderbuffer.c
@@ -1086,7 +1086,8 @@ ReorderBufferFreeSnap(ReorderBuffer *cache, Snapshot snap)
  * assigned via ReorderBufferCommitChild.
  */
 void
-ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn, RepNodeId origin)
+ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn,
+					RepNodeId origin, TimestampTz commit_time)
 {
 	ReorderBufferTXN *txn = ReorderBufferTXNByXid(cache, xid, false, NULL);
 	ReorderBufferIterTXNState *iterstate = NULL;
@@ -1102,6 +1103,7 @@ ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn, Rep
 
 	txn->last_lsn = lsn;
 	txn->origin_id = origin;
+	txn->commit_time = commit_time;
 
 	/* serialize the last bunch of changes if we need start earlier anyway */
 	if (txn->nentries_mem != txn->nentries)

amenonsen/postgres

src/backend/replication/logical/reorderbuffer.c
@@ -1148,7 +1148,8 @@ ReorderBufferFreeSnap(ReorderBuffer *buffer, Snapshot snap)
  * assigned via ReorderBufferCommitChild.
  */
 void
-ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn, RepNodeId origin)
+ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn,
+					RepNodeId origin, TimestampTz commit_time)
 {
 	ReorderBufferTXN *txn;
 	ReorderBufferIterTXNState *iterstate = NULL;
@@ -1167,6 +1168,7 @@ ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn, Re
 
 	txn->last_lsn = lsn;
 	txn->origin_id = origin;
+	txn->commit_time = commit_time;
 
 	/* serialize the last bunch of changes if we need start earlier anyway */
 	if (txn->nentries_mem != txn->nentries)