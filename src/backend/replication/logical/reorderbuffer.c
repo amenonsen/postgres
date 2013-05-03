2ndQuadrant/bdr

bdr: Introduce "replication_identifiers" to keep track of remote nodes
Replication identifiers can be used to track & lookup remote nodes identified
via (sysid, tlid, remote_dbid, local_dbid, name) and map that tuple to a local
uint16.
Keyed by that replication identifier the remote lsn and the local lsn is
tracked in a crash safe manner.

Support for tracking that via output plugins is added as well.

Needs a catversion bump.

src/backend/replication/logical/reorderbuffer.c
@@ -1086,7 +1086,7 @@ ReorderBufferFreeSnap(ReorderBuffer *cache, Snapshot snap)
  * assigned via ReorderBufferCommitChild.
  */
 void
-ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn)
+ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn, RepNodeId origin)
 {
 	ReorderBufferTXN *txn = ReorderBufferTXNByXid(cache, xid, false, NULL);
 	ReorderBufferIterTXNState *iterstate = NULL;
@@ -1101,6 +1101,7 @@ ReorderBufferCommit(ReorderBuffer *cache, TransactionId xid, XLogRecPtr lsn)
 		return;
 
 	txn->last_lsn = lsn;
+	txn->origin_id = origin;
 
 	/* serialize the last bunch of changes if we need start earlier anyway */
 	if (txn->nentries_mem != txn->nentries)

amenonsen/postgres

src/backend/replication/logical/reorderbuffer.c
@@ -1148,7 +1148,7 @@ ReorderBufferFreeSnap(ReorderBuffer *buffer, Snapshot snap)
  * assigned via ReorderBufferCommitChild.
  */
 void
-ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn)
+ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn, RepNodeId origin)
 {
 	ReorderBufferTXN *txn;
 	ReorderBufferIterTXNState *iterstate = NULL;
@@ -1166,6 +1166,7 @@ ReorderBufferCommit(ReorderBuffer *buffer, TransactionId xid, XLogRecPtr lsn)
 		return;
 
 	txn->last_lsn = lsn;
+	txn->origin_id = origin;
 
 	/* serialize the last bunch of changes if we need start earlier anyway */
 	if (txn->nentries_mem != txn->nentries)